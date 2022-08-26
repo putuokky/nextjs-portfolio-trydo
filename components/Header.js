bb61b6f0ac72771f07402bcedd70cec1e6e45889
Okky Maheswara <okkymahes@gmail.com>
2022-08-27 00:07:43 +0800

    buat hero text

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,45 @@
 export default function Header() {
   return (
     <div className="home">
-      <h1>Header</h1>
+      <div className="home__bg">
+        <div className="header d__flex align__items__center pxy__30">
+          <div className="logo">
+            <img src="/img/logo.png" alt="logo" />
+          </div>
+          <div className='navigation pxy__30'>
+            <ul className='navbar d__flex'>
+              <a href='#Home'>
+                <li className='nav__items mx__15'>Home</li>
+              </a>
+              <a href='#About'>
+                <li className='nav__items mx__15'>About</li>
+              </a>
+              <a href='#Services'>
+                <li className='nav__items mx__15'>Services</li>
+              </a>
+              <a href='#Blog'>
+                <li className='nav__items mx__15'>Blog</li>
+              </a>
+              <a href='#Contact'>
+                <li className='nav__items mx__15'>Contact</li>
+              </a>
+            </ul>
+          </div>
+        </div>
+        {/* Home Content */}
+        <div className="container">
+          <div className="home__content">
+            <div className="home__meta">
+              <h1 className="home__text pz__10">
+                WELCOME TO MY WORLD
+              </h1>
+              <h2 className="home__text pz__10">
+                Hi,I'm John Doe
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>ini</div>
+    <div>
+      <Header />
+    </div>
   )
 }
