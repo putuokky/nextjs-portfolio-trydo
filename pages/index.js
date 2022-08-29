8e4a46abaf627f41ded93184286a79c4f3e9633d
Okky Maheswara <okkymahes@gmail.com>
2022-08-29 11:08:40 +0800

    clearr service

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Header from "../components/Header";
+import Service from "../components/Service";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <About />
+      <Service />
     </div>
   )
 }
