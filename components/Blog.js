b6ff329aaede467ed57c02edf744469de904e270
Okky Maheswara <okkymahes@gmail.com>
2022-08-30 20:31:04 +0800

    blog clear

--- components/Blog.js
+++ components/Blog.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import blog1 from "../public/img/blog-01.jpg";
+import blog2 from "../public/img/blog-02.jpg";
+import blog3 from "../public/img/blog-03.jpg";
 
 export default function Blog() {
   return (
@@ -16,6 +18,38 @@ export default function Blog() {
               <div className="project__box__img pointer relative">
                 <div className="project__img__box">
                   <img src="/img/blog-01.jpg" alt="" className="project__img" />
+                  {/* <Image className="project__img" alt="" src={blog1} /> */}
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="blog_meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">Getting tickets to the big show</h4>
+                <a href="#" className="blog project__btn btn">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src="/img/blog-02.jpg" alt="" className="project__img" />
+                  {/* <Image className="project__img" alt="" src={blog1} /> */}
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="blog_meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">Getting tickets to the big show</h4>
+                <a href="#" className="blog project__btn btn">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src="/img/blog-03.jpg" alt="" className="project__img" />
                   {/* <Image className="project__img" alt="" src={blog1} /> */}
                 </div>
                 <div className="mask__effect"></div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import About from "../components/About";
-import Blog from "../components/Blog";
-import Header from "../components/Header";
-import Project from "../components/Project";
-import Service from "../components/Service";
+import About from "@components/About";
+import Blog from "@components/Blog";
+import Header from "@components/Header";
+import Project from "@components/Project";
+import Service from "@components/Service";
 
 export default function Home() {
   return (
