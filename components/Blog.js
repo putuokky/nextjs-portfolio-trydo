afb61b692aabd52b325554324a075124f2a86e88
Okky Maheswara <okkymahes@gmail.com>
2022-09-03 10:48:51 +0800

    lallaa

--- components/Blog.js
+++ components/Blog.js
@@ -5,64 +5,61 @@ import blog3 from "../public/img/blog-03.jpg";
 
 export default function Blog() {
   return (
-    <div className="blog component__space" id="Blog">
-      <div className="heading">
-        <h1 className="heading">My Latest News</h1>
-        <p className="heading p__color">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, hic.</p>
-        <p className="heading p__color">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    <section className="py-24" id="blog">
+      <div>
+        <h1 className="text-6xl text-center font-poppins font-bold mb-2.5">My Latest News</h1>
+        <p className="mb-4 text-lg leading-5 text-center text-more font-poppins">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, hic.</p>
+        <p className="mb-4 text-lg leading-5 text-center text-more font-poppins">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
       </div>
-      <div className="container">
-        <div className="row">
-          <div className="col__3">
-            <div className="project__box pointer relative">
-              <div className="project__box__img pointer relative">
-                <div className="project__img__box">
-                  <img src="/img/blog-01.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={blog1} /> */}
+      <div className="container w-11/12 mx-auto">
+        <div className="flex flex-wrap items-start justify-center w-full overflow-hidden">
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={blog1} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="blog_meta absolute">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
+              <div className="w-full h-full top-0 left-0 flex items-center justify-end px-5 flex-col flex-wrap rounded-xl overflow-hidden absolute">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
                 <a href="#" className="blog project__btn btn">Read More</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="project__box pointer relative">
-              <div className="project__box__img pointer relative">
-                <div className="project__img__box">
-                  <img src="/img/blog-02.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={blog1} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={blog2} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="blog_meta absolute">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
+              <div className="w-full h-full top-0 left-0 flex items-center justify-end px-5 flex-col flex-wrap rounded-xl overflow-hidden absolute">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
                 <a href="#" className="blog project__btn btn">Read More</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="project__box pointer relative">
-              <div className="project__box__img pointer relative">
-                <div className="project__img__box">
-                  <img src="/img/blog-03.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={blog1} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={blog3} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="blog_meta absolute">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="blog project__btn btn">Read More</a>
+              <div className="w-full h-full top-0 left-0 flex items-center justify-end px-5 flex-col flex-wrap rounded-xl overflow-hidden absolute">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 mb-12 text-lg font-medium rounded-md border-2 border-solid border-primary bg-primary text-white uppercase tracking-widest transition-all duration-300 ease-linear h-12 w-44 mx-2.5 hover:mb-10">Read More</a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
--- components/Project.js
+++ components/Project.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import project1 from "../public/img/portfolio-1.jpg";
 import project2 from "../public/img/portfolio-2.jpg";
 import project3 from "../public/img/portfolio-3.jpg";
@@ -12,106 +13,102 @@ export default function Project() {
         <p className="mb-4 text-lg leading-5 text-center text-more font-poppins">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, hic.</p>
         <p className="mb-4 text-lg leading-5 text-center text-more font-poppins">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
       </div>
-      <div className="container">
-        <div className="row">
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-1.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project1} /> */}
+      <div className="container w-11/12 mx-auto">
+        <div className="flex flex-wrap items-start justify-center w-full overflow-hidden">
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project1} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-3.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project3} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project3} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-2.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project2} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project2} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-4.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project4} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project4} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-1.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project1} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project1} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="col__3">
-            <div className="relative project__box pointer">
-              <div className="relative project__box__img pointer">
-                <div className="project__img__box">
-                  <img src="/img/portfolio-3.jpg" alt="" className="project__img" />
-                  {/* <Image className="project__img" alt="" src={project2} /> */}
+          <div className="flex flex-col justify-center w-1/3 p-5 mt-5">
+            <div className="relative hover:inline hover:transform hover:scale-110 cursor-pointer">
+              <div className="relative cursor-pointer">
+                <div>
+                  <Image className="rounded-xl w-full object-cover transition duration-300 opacity-70 relative after:content-none after:w-full after:h-full after:bg-black after:z-[1] after:opacity-10" alt="" src={project2} />
                 </div>
-                <div className="mask__effect"></div>
+                <div className="w-full h-full absolute top-0 left-0 bg-primary rounded-xl transition duration-300 opacity-30 hidden hover:inline hover:transform hover:scale-110"></div>
               </div>
-              <div className="absolute project__meta">
-                <h5 className="project__text">Development</h5>
-                <h4 className="project__text">Getting tickets to the big show</h4>
-                <a href="#" className="project__btn">View Details</a>
+              <div className="absolute top-0 left-0 flex h-full items-center justify-center flex-col flex-wrap rounded-xl w-full">
+                <h5 className="font-normal text-xl pb-5">Development</h5>
+                <h4 className="text-2xl font-poppins font-bold pb-5">Getting tickets to the big show</h4>
+                <a href="#" className="py-3 px-9 border-2 border-solid border-more rounded-md transition duration-300 ease-in-out hover:bg-primary hover:border-primary">View Details</a>
               </div>
             </div>
           </div>
-          <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
-            <button className="view__more pointer btn">View More</button>
+          <div className="flex items-center justify-center p-8">
+            <Link href="#">
+              <a className="hover:bg-white hover:text-primary py-4 px-5 font-medium rounded-md border-2 border-solid border-primary bg-primary text-white uppercase tracking-widest transition-all duration-300 ease-linear cursor-pointer">View More</a>
+            </Link>
           </div>
         </div>
       </div>
